a1eb0b98c5fc05ba1f66c0eb722f1bb26d1af792
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-23 07:46:46 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 200402;
+let accountEmail = "ujjwal@google.com";
+var accountPassword = "12345";
+
+accountCity = "delhi";
+// accountId = 112233; //not allowed
+
+// let,var are changed after initialisation
+accountEmail = "diya@gmail.com";
+accountPassword = "2345";
+accountCity = "dehradoon";
+
+// prefer not to use var b'coz od issue in block and functional scope
+console.log(accountId);
+//if there are more than 1 variable to print then use console.table instead of clog
+console.table([accountId,accountEmail,accountPassword,accountCity]);
