3458f5576d7ae4479ec66eed63edd99535e1c69e
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-24 07:52:37 +0000

    basic operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,27 @@
+let a = 3;
+let negA = -a;
+// console.log(negA);
+
+// some common operations use in js
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*5); //10
+// console.log(2/4); //0.5 
+// console.log(2%3); //2
+// console.log(2**2); //4
+
+let str1 = "Hello";
+let str2 = " Guys";
+// console.log(str1+str2); // Hello Guys
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);//101
+
+
+
