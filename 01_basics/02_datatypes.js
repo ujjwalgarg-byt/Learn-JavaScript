69e939f252c194b6b65b8c4d7918fcdb8d6b721e
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-24 07:01:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3); //code readablity should be high
+console.log(3+3);
+
+// data types
+let name = "ujjwal";
+let age = 21;
+let isLogged = true;
+let state;
+let a=null;//null datatype
+
+//number => 2 to power 53
+//bigInt => use for big numbers like trading
+//string => use in ""
+//boolean => ture/false
+//null => stand alone value
+//undefined => use when value is not defined
+//sybol => use for uniqueness
+
+//objects
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
