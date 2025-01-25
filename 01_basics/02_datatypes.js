625fff3a30f465d349bb1d24e567995d3f46ce2c
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-25 07:56:21 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,12 +6,8 @@ console.log(3
     +3); //code readablity should be high
 console.log(3+3);
 
-// data types
-let name = "ujjwal";
-let age = 21;
-let isLogged = true;
-let state;
-let a=null;//null datatype
+// data types are of two types primitive and non-primitive(refrence type)
+//Primitive datatypes:- they are call by value and have 7 types
 
 //number => 2 to power 53
 //bigInt => use for big numbers like trading
@@ -21,8 +17,43 @@ let a=null;//null datatype
 //undefined => use when value is not defined
 //sybol => use for uniqueness
 
-//objects
+// example of primitive type
+let name = "ujjwal"; //string
+let age = 21; //number
+let isLogged = true; //boolean
+let state; //undefined
+let a=null;//null 
+const bigNumber = 3457927353728272n; //bigInt
+const id = Symbol('123'); //symbol
+
+
+// Non-Primitive datatypes:- 3 types
+
+// Arays
+// Objects
+// Functions
+
+//example of non-primitive types
+const heros = ['shaktiman','naagraj','juniar-g']; // arays
+
+const myObj={
+    name:"ujjjwal",
+    age:22,
+    class:"b.tech",
+}; // object
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}; //functions
+
+// for checking the type of any datatype use (typeof) like below
 
 console.log(typeof null);//object
 console.log(typeof undefined);//undefined
+console.log(typeof array);// object
+console.log(typeof myFunction);//object
+console.log(typeof object);//object
+console.log(typeof bigInt);//bigInt
+console.log(typeof Symbol);//Symbol
 
