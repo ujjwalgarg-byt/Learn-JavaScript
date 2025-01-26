8752893e76ad7f17209da39cbfd9ec70d8e9935b
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-26 07:42:42 +0000

    strings and it's methods

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,30 @@
+// fisrt method of declaring string
+const name = 'Ujjwal';
+const age = 21;
+
+console.log(`Hello my name is ${name} and my age is ${age}`);// this called string interpolation
+
+// another method of declaring string
+
+const myName = new String("ujjwal garg");
+// method use in string
+console.log(myName[0]); //u
+console.log(myName.length);//11
+console.log(myName.toUpperCase());//UJJWAL GARG
+console.log(myName.charAt(3));//w
+console.log(myName.__proto__);
+console.log(myName.indexOf("w"));//3
+console.log(myName.substring(0,5));//ujjwa
+console.log(myName.slice(7,10));//gar
+
+console.log(myName.replace('garg','aggarwal'));//ujjwal aggarwal
+console.log(myName.includes('hey'));//false
+
+const newString = "  hello  ";
+console.log(newString);// o/p--(  hello  )
+console.log(newString.trim());// remove extra spaces -- o/p is hello
+
+const newName= "ujjwal-21-b.tech-c.s.e";
+console.log(newName.split('-'));// make array according to - or given parameter
+//['ujjwal','21','b.tech','c.s.e']
+
