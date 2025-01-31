7e9c30695078b3e4d6aacb22fda3438e46949e47
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-01-31 08:16:20 +0000

    Object part2

---
+++ 02_basics/03_ObjectPart2.js
@@ -0,0 +1,43 @@
+// Object declaration by constructors 
+//Singleton Objects creation
+// const  snapchatUser = new Object();  // singleton object
+
+const snapchatUser ={};// non singleton obj
+snapchatUser.id = "123abc";
+snapchatUser.name = 'abc';
+snapchatUser.isLoggedIn = false;
+// console.log(snapchatUser);
+
+// methods in objects
+console.log(Object.keys(snapchatUser)); //return keys of object(id,name,isLoggedIn)
+console.log(Object.values(snapchatUser));//return values of object's keys("123abc",'abc',false)
+console.log(Object.entries(snapchatUser));//return arrays of key and values
+console.log(snapchatUser.hasOwnProperty('id'));// check this property belong to obj or not
+
+//object chaining
+const userObj={
+    email:"user@gmail.com",
+    fullname:{
+        username:{
+            firstname:"ujjwal",
+            lastname:"garg",
+        },
+    },
+};
+console.log(userObj.fullname.username.firstname);//ujjwal
+console.log(userObj.fullname.username.lastname);//garg
+
+// merging two or more objects
+const obj1={1:"a",2:"b"};
+const obj2={3:"a",4:"b"};
+
+//1:-
+const obj3 = Object.assign({},obj1,obj2);
+console.log(obj3);//{1:"a",2:"b",3:"a",4:"b"}
+
+//2:-
+const obj4 = {...obj1,...obj2};
+console.log(obj4); //{1:"a",2:"b",3:"a",4:"b"}
+
+
+
