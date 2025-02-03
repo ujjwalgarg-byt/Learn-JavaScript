066a50c72e15dcbf02d785992eb9ce60683d1eea
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-02-03 07:10:14 +0000

    Scopes

--- 03_Functions/02_scope.js
+++ 03_Functions/02_scope.js
@@ -17,7 +17,32 @@ if(true){
 }
 console.log(a);//300
 console.log(c);//22 b'coz var is changeble within scope
+// block scope take value from global scope but it does not give it's value or leak value of it's own
+
+//++++ nested scope : -- function inside a function
+function first(){
+    const username = "ujjwal";
+    function second(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); //it is not access here b'coz child function can access the variable of it's parent function but parent function don't
+    second();
+    
+}
+first();
 
 
+//++++++++++++++ Interesting Concept  ++++++++++++++++++
 
+console.log(addOne(5));//here it is executed
+function addOne(num){
+    return num+1;
+}
 
+// or
+addTwo(7); // here it does not execute b'coz of declaration method which is called hosting
+const addTwo=function(num){
+    return num+2;
+}
