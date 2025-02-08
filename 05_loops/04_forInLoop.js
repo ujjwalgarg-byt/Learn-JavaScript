0f10630b343c2da322e1bad2b4b9d11fa1bf21a4
Ujjwal Garg <143330622+ujjwalgarg-byt@users.noreply.github.com>
2025-02-08 07:03:19 +0000

    for each loop

---
+++ 05_loops/04_forInLoop.js
@@ -0,0 +1,30 @@
+// for in loop
+//ex for objects
+const myObj={
+    js:"javascript",
+    py:"python",
+    rb:"ruby",
+    cpp:"c++",
+};
+for(const keys in myObj){
+    console.log(`${keys} is shortcut for ${myObj[keys]}`);
+    //js is shortcut for javascript
+    //py is shortcut for python
+    //rb is shortcut for ruby
+    //cpp is shortcut for c++
+}
+
+// ex for array
+const myArray = [1,2,3,4,5];
+for (const key in myArray) {
+   console.log(myArray[key]);//1,2,3,4,5
+   
+}
+// ex for string:-
+const myName = "ujjwal garg";
+for(const key in myName){
+    console.log(myName[key]);//u,j,j,w,a,l g,a,r,g
+
+}
+
+//+++ for in loop is not use for maps or maps is not iteratable in for in loop
